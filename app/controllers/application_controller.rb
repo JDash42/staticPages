e159a6bea327346db92b096116a257d53602e37e
JDash42 <triggeredjd@gmail.com>
2019-09-03 00:06:06 +0000

    Update Gemfile for Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def home
+    render html: "welcome home"
+  end
+  
+  def basics
+  end
+  
+  def ruby
+  end
+  
+  def git
+  end
+
 end
