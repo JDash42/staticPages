f06ba1e791f6437d0c69c0656ba6035af5f9577e
JDash42 <triggeredjd@gmail.com>
2019-09-03 05:59:53 +0000

    pages generated

--- app/controllers/application_controller.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-  def home
-    render html: "welcome home"
-  end
-  
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/basics'
+
+  get 'pages/ruby'
+
+  get 'pages/git'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#home'
 end
