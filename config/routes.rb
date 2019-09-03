970a9f78884fba139a7ff4ae6d7876e2d011a089
JDash42 <triggeredjd@gmail.com>
2019-09-03 07:13:13 +0000

    heroku command lost

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+  root 'pages#home'
   get 'pages/home'
 
   get 'pages/basics'
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   get 'pages/ruby'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#home'
+  
 end
